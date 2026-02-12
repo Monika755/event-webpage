96166587437493cd513e84f331005119a868fe1b
Monika <monikabaghumyan0@gmail.com>
2026-02-12 17:04:35 +0400

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Card.css";     
+import heroImg from "../assets/heroImg.jpg"
+
+
+export default function Hero () { 
+ return ( 
+    <section className="hero"> 
+     <img   
+     src={heroImg} 
+     alt="Hero Image" 
+     className="hero-img" 
+      /> 
+        <div className="hero-text"> 
+            <h1>Discover Amazing Events</h1> 
+            <p>Find the best events heppening near you!</p> 
+            <button>Explore Events</button>
+        </div>
+     </section>
+ );
+}
